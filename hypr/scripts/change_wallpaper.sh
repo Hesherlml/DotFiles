34922a17c56b959833fb64bf973eb093cc387d60
Hesherlml <axiel.lml@gmail.com>
2025-07-21 09:06:51 +0200

    Add files via upload

---
+++ hypr/scripts/change_wallpaper.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Esperar a que hyprpaper cree su socket
+while ! hyprctl hyprpaper ls >/dev/null 2>&1
+do
+  sleep 0.5
+done
+
+wallpaper_directory=/home/solrojo/.config/wallpaper
+wallpaper_history=/home/solrojo/.config/hypr/scripts/wallpaper_history.txt
+
+
+#Crear el archivo del historial si no exite
+touch "$wallpaper_history"
+
+
+#Leer los ultimos 4 wallpapers utilizados
+wallpaper_last=($(tail -n 4 "$wallpaper_history"))
+
+#Busca wallpapers que no se encuentren en el historial
+#Nota: shuf selecciona 1 wallpaper al azar
+#Nota: grep -Ff invierte la busqueda (v) y busca patrones fijos de string (F) en el archivo (f)
+if [ ${#wallpaper_last[@]} -eq 0 ]
+then
+    wallpaper=$(find "$wallpaper_directory" -type f | shuf -n 1)
+else
+    wallpaper=$(find "$wallpaper_directory" -type f | grep -v -Ff  "$wallpaper_history" | shuf -n 1)
+fi
+
+#Cargar el wallpaper
+hyprctl hyprpaper preload "$wallpaper"
+hyprctl hyprpaper wallpaper , "$wallpaper"
+sleep 1
+hyprctl hyprpaper unload unused
+
+#Actualiza el archivo historial
+echo "$wallpaper" >> "$wallpaper_history"
+
+#Mantener solo los 4 ultimos wallpapers en el historial
+tail -n 4 "$wallpaper_history" > "$wallpaper_history.tmp" && mv "$wallpaper_history.tmp" "$wallpaper_history"
+
+exit 0
