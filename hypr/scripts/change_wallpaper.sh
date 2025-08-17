a5062e3233016b8ebfc257f33b55988d7afec353
Solrojo <axiel.lml@gmail.com>
2025-08-17 10:47:13 +0200

    Update change_wallpaper.sh
    > Added english comments

--- hypr/scripts/change_wallpaper.sh
+++ hypr/scripts/change_wallpaper.sh
@@ -4,16 +4,16 @@ wallpaper_directory=~/.config/wallpaper
 wallpaper_history=~/.config/hypr/scripts/wallpaper_history.txt
 
 
-#Crear el archivo del historial si no exite
+#If the history file doesn't exist, it's created
 touch "$wallpaper_history"
 
 
-#Leer los ultimos 4 wallpapers utilizados
-wallpaper_last=($(tail -n 32 "$wallpaper_history"))
+#Read the last used wallpapers
+wallpaper_last=($(tail -n 4 "$wallpaper_history"))
 
-#Busca wallpapers que no se encuentren en el historial
-#Nota: shuf selecciona 1 wallpaper al azar
-#Nota: grep -Ff invierte la busqueda (v) y busca patrones fijos de string (F) en el archivo (f)
+#Looks for wallpapers not found in the history file
+#Note: shuf picks 1 random wallpaper 
+#Note: grep -Ff reverse the search (v) and looks for string patterns (F) in the file (f)
 if [ ${#wallpaper_last[@]} -eq 0 ]
 then
     wallpaper=$(find "$wallpaper_directory" -type f | shuf -n 1)
@@ -21,16 +21,16 @@ else
     wallpaper=$(find "$wallpaper_directory" -type f | grep -v -Ff  "$wallpaper_history" | shuf -n 1)
 fi
 
-#Cargar el wallpaper
+#Load wallpaper
 hyprctl hyprpaper preload "$wallpaper"
 hyprctl hyprpaper wallpaper , "$wallpaper"
 sleep 1
 hyprctl hyprpaper unload unused
 
-#Actualiza el archivo historial
+#Update history file with current wallpaper
 echo "$wallpaper" >> "$wallpaper_history"
 
-#Mantener solo los 4 ultimos wallpapers en el historial
-tail -n 32 "$wallpaper_history" > "$wallpaper_history.tmp" && mv "$wallpaper_history.tmp" "$wallpaper_history"
+#Only preserve the last 4 used wallpapers in the history
+tail -n 4 "$wallpaper_history" > "$wallpaper_history.tmp" && mv "$wallpaper_history.tmp" "$wallpaper_history"
 
 exit 0
