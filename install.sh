ad0463d212ad59d74220ca97d550ac21019c38a6
Solrojo <axiel.lml@gmail.com>
2025-08-17 11:06:48 +0200

    Update install.sh
    > Added signature
    > Updated comments for improved clarity

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-# Script to install Solrojo-Script's DotFiles
-# Dotfiles include : hyprland, hyprpaper, hyprlock, waybar, wlogout, wofi and kitty
+# #######################################################################################
+# # /* ---- 🐧 https://github.com/Solrojo-Script 🐧 ---- */  #
+# #######################################################################################
 #
-#This includes the scripts for changing the wallpaper with hyprpaper (~/.config/hypr/scripts) + wallpapers (~/.config/wallpaper).
+# Installation Script for Solrojo-Script's DotFiles
+# This collection includes configurations for: hyprland, hyprpaper, hyprlock, waybar, wlogout, wofi, and kitty.
 #
-#This also includes the folder icons of Adwaita++ with Suru++
+# It features scripts for changing wallpapers using hyprpaper, located in ~/.config/hypr/scripts, along with the wallpapers stored in ~/.config/wallpaper.
 #
-# WARNING : Make sure you have the following applications and fonts already installed before executing this script : hyprland hyprpaper hyprlock hyprpolkitagent waybar wlogout wofi kitty nwg-look xdg-desktop-portal-hyprland xdg-desktop-portal-gtk nautilus git ttf-font-awesome ttf-jetbrains-mono-nerd wget
+# Additionally, it contains folder icons from Adwaita++ combined with Suru++.
+#
+# WARNING: Before running this script, ensure that the following applications and fonts are installed: hyprland, hyprpaper, hyprlock, hyprpolkitagent, waybar, wlogout, wofi, kitty, nwg-look, xdg-desktop-portal-hyprland, xdg-desktop-portal-gtk, nautilus, git, ttf-font-awesome, ttf-jetbrains-mono-nerd, and wget.
 
 #Copying files
 if [ ! -d ~/.config/hypr ]; then
@@ -36,16 +40,16 @@ if [ ! -d ~/.config/wofi ]; then
 fi
 cp -r wofi/* ~/.config/wofi/
 
-#Making scripts for wallpaper executable
+# Setting the wallpaper scripts to be executable
 cd ~/.config/hypr/scripts
 chmod +x change_wallpaper.sh change_wallpaper_timer.sh change_wallpaper_wrapper.sh
 
 cd /tmp/
 rm -rf DotFiles
 
-#WARNING : YOU'LL NEED nwg-look, xdg-desktop-portal-gtk and xdg-desktop-portal-hyprland FOR THE NEXT PART 
+# WARNING: The following packages are required for the next steps: nwg-look, xdg-desktop-portal-gtk, and xdg-desktop-portal-hyprland.
 
-#Icons install (Suru ++ with Adwaita++)
+# Installing Icons (Suru++ combined with Adwaita++)
 git clone https://github.com/Bonandry/adwaita-plus.git
 cd adwaita-plus
 chmod +x install.sh && ./install.sh
@@ -58,7 +62,7 @@ chmod +x install.sh && ./install.sh
 cd ..
 rm -rf suru-plus-folders
 
-#Aplying icon theme and 'GOING DARK'
+# Applying the icon theme and switching to 'Dark Mode'
 suru-plus-folders -C purple --theme Adwaita++
 gsettings set org.gnome.desktop.interface icon-theme 'Adwaita++'
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
