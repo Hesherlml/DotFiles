355187e1412716dc4d3ca7efba3e2f8a3295afdf
Solrojo <axiel.lml@gmail.com>
2025-08-17 10:48:47 +0200

    Update change_wallpaper_timer.sh
    > Added english comments

--- hypr/scripts/change_wallpaper_timer.sh
+++ hypr/scripts/change_wallpaper_timer.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# Esperar a que hyprpaper cree su socket
+# Waits for hyprpaper
 while ! hyprctl hyprpaper ls >/dev/null 2>&1
 do
   sleep 0.5
 done      
 
 
-# Bucle infinito para cambiar fondo cada 10 minutos
+# Infinite loop : changes wallpaper every 10 minutes
 while true
 do
 	~/.config/hypr/scripts/change_wallpaper.sh
-	sleep 600 #segundos
+	sleep 600 #seconds
 done
 
 
